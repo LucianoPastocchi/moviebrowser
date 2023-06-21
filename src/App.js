1201193e745bbf888fbd99f87b931e826983ff6a
System Administrator <root@MacBook-Air.fibertel.com.ar>
2023-06-21 18:54:15 -0300

    API Key TMDB

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import SearchView from "./components/SearchView";
 function App() {
   const [searchResults, setSearchResults] = useState([]); //store in the overall app
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    console.log(searchText);
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=74d33cb230b329de3dbba1fd04d2268a&language=en-US&query=${searchText}&page=1&include_adult=false`
+    )
+      .then((response) => response.json())
+      .then((data) => setSearchResults(data.results));
+  }, [searchText]);
+
   return (
     <div>
       <NavBar searchText={searchText} setSearchText={setSearchText} />
--- src/components/SearchView.jsx
+++ src/components/SearchView.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Hero from "./Hero";
 
+// TMDB API 74d33cb230b329de3dbba1fd04d2268a
+// example link for movie search https://api.themoviedb.org/3/search/keyword
+
 const SearchView = ({ keyword, searchResults }) => {
   const title = `You are searching for ${keyword}`;
   return (
